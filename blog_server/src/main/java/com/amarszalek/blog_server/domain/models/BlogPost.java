771cd3ef431c1f1c24b49af3b080d61b160c0553
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2019-03-24 20:46:41 +0100

    basic properties, BlogPost entity

---
+++ blog_server/src/main/java/com/amarszalek/blog_server/domain/models/BlogPost.java
@@ -0,0 +1,26 @@
+package com.amarszalek.blog_server.domain.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class BlogPost {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String authorName;
+    private String authorUserName;
+    private String content;
+    private Date modificationDate;
+    private String subject;
+    @ElementCollection
+    @CollectionTable(name="post_tags", joinColumns=@JoinColumn(name="post_id"))
+    private List<String> tags = new ArrayList<>();
+}
