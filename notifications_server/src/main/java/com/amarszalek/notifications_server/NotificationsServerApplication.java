b20245694b2944e6cebdcb0dc81126a45c96831f
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2019-03-24 20:20:12 +0100

    first commit

---
+++ notifications_server/src/main/java/com/amarszalek/notifications_server/NotificationsServerApplication.java
@@ -0,0 +1,13 @@
+package com.amarszalek.notifications_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsServerApplication.class, args);
+	}
+
+}
