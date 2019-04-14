786f4dacd7c3d134585170979170ecfd59003515
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2019-04-14 12:40:53 +0200

    EmailServiceTest.shouldInvokeSendMessaeMethodWithExpectMessage

--- notifications_server/src/test/java/com/amarszalek/notifications_server/domain/utils/EmailServiceTest.java
+++ notifications_server/src/test/java/com/amarszalek/notifications_server/domain/utils/EmailServiceTest.java
@@ -2,7 +2,6 @@ package com.amarszalek.notifications_server.domain.utils;
 
 import com.amarszalek.notifications_server.NotificationsServerApplication;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -13,9 +12,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMultipart;
+import java.io.IOException;
 import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -24,23 +23,31 @@ import static org.powermock.api.mockito.PowerMockito.doNothing;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = NotificationsServerApplication.class)
 @RunWith(SpringRunner.class)
 public class EmailServiceTest extends PowerMockTestCase {
+
+    private String message = "example message";
+    private String emailAddress = "emailAddress@gmail.com";
+
     @MockBean
     private TransportWrapper transportWrapper;
 
     @Autowired
     private EmailService emailService;
 
-
-    @Before
-    public void setUp() {
-
-    }
-
     @Test
     public void shouldInvokeTransportWrapperSendMessage() throws MessagingException {
         //given
-        String message = "example message";
-        String emailAddress = "emailAddress@gmail.com";
+        doNothing().when(transportWrapper).sendMessage(isA(Message.class));
+        ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
+        //when
+        emailService.sendMail(message, emailAddress);
+        //then
+        verify(transportWrapper, times(1)).sendMessage(messageArgumentCaptor.capture());
+    }
+
+    @Test
+    public void shouldInvokeSendMessageMethodWithExpectedMessage() throws MessagingException {
+        //given
+        String contentMessage = "";
         doNothing().when(transportWrapper).sendMessage(isA(Message.class));
         ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
         //when
@@ -48,6 +55,13 @@ public class EmailServiceTest extends PowerMockTestCase {
         //then
         verify(transportWrapper, times(1)).sendMessage(messageArgumentCaptor.capture());
         List<Message> allValues = messageArgumentCaptor.getAllValues();
-        Assert.assertEquals(message, message);
+        try {
+            MimeMultipart content = (MimeMultipart) allValues.get(0).getDataHandler().getContent();
+            contentMessage = content.getBodyPart(0).getDataHandler().getContent().toString();
+            System.out.println("");
+        } catch (IOException e) {
+            Assert.fail("Unexpected message");
+        }
+        Assert.assertEquals(message, contentMessage);
     }
 }
